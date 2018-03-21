1431eb7df12c1c8f6c24e71712cef4355c52c492
garvinipkiss <garvinipkiss@gmail.com>
2018-03-21 20:11:54 +0300

    working progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,4 +4,4 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './account/account.component';

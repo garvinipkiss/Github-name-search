77d48116395df8f45d13ec807a101a3334e06fc9
garvinipkiss <garvinipkiss@gmail.com>
2018-03-21 20:06:11 +0300

    working progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import{ GithubService} from './github/github.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+
+    ],
+    providers: [GithubService],
+      bootstrap: [AppComponent]
+    })
+    export class AppModule { }

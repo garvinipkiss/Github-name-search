45ecacd3eec4b79684ed7cddfe65813ccf8f128b
garvinipkiss <garvinipkiss@gmail.com>
2018-03-21 21:13:31 +0300

    on progress

--- src/app/account/account.component.spec.ts
+++ src/app/account/account.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AccountComponent } from './account.component';
+import { accountComponent } from './account.component';
 
-describe('AccountComponent', () => {
-  let component: AccountComponent;
-  let fixture: ComponentFixture<AccountComponent>;
+describe('accountComponent', () => {
+  let component: accountComponent;
+  let fixture: ComponentFixture<accountComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccountComponent ]
+      declarations: [ accountComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AccountComponent);
+    fixture = TestBed.createComponent(accountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

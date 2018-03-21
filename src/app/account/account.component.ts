98e35a3f017fc316d8f45be374a1412475033b3e
garvinipkiss <garvinipkiss@gmail.com>
2018-03-21 21:15:37 +0300

    on progress

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../github.service';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
-
-  constructor() { }
-
+export class accountComponent implements OnInit {
+  user:any=[];
+  repos:any=[];
+  username:string;
+  constructor(private _githubService: GithubService) {
+    this._githubService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    })
+    this._githubService.getRepos().subscribe(repos => {
+      //console.log(user);
+      this.repos = repos;
+    })
+  }
   ngOnInit() {
   }
+   searchUser(){
+     this._githubService.updateUser(this.username);
+     this._githubService.getUser().subscribe(user => {
+       this.user = user;
+     })
+     //console.log('It works');
+     this._githubService.getRepos().subscribe(repos => {
+       //console.log(user);
+       this.repos = repos;
+     })
 
+   }
 }
